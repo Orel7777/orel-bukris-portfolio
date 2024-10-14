4b19f3c7272aad6145dea9ac46b0431c08c6bf65
Orel bukris <orelbukris7777@gmail.com>
2024-10-14 09:40:50 +0300

    second

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Orel Bukris</h1>
+    <main className="relative bg-black-100 flex-justify-center
+    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>Orel Bukris portfolio</h1>
       </div>
     </main>
   );
