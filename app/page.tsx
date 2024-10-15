c791d9a63e458dcbce04008c1ac98d309697a91c
Orel bukris <orelbukris7777@gmail.com>
2024-10-16 00:23:33 +0300

    go

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
+     
       </div>
     </main>
   );
