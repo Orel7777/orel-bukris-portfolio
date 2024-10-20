534880cdbfc6fdc6e62671d287b5a032c78a5c23
Orel bukris <orelbukris7777@gmail.com>
2024-10-20 16:27:55 +0300

    var 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Card3D } from "@/components/Card3D";
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+// import { FaHome } from "react-icons/fa";
 
 
 export default function Home() {
@@ -9,10 +9,10 @@ export default function Home() {
     <main className="relative bg-black-100 flex-justify-center
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav 
+        {/* <FloatingNav 
         navItems={[
           {name:'Home',link:'/',icon:<FaHome />}
-        ]} />
+        ]} /> */}
         <Hero />
         <Card3D/>
       </div>
