cfbbe8a188c106d2f50e37ec0765fa706b579e44
Orel bukris <orelbukris7777@gmail.com>
2024-10-27 20:06:20 +0200

    ver

--- data/index.ts
+++ data/index.ts
@@ -9,9 +9,9 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "Client-focused open communication",
       description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:h-[65vh]",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:h-[65vh] ",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
       img: "/pic_computer.png",
