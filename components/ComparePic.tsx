61a602b62100c1723e7d6dab71484b62cbacff4a
Orel bukris <orelbukris7777@gmail.com>
2024-10-29 22:47:44 +0200

    change

--- components/ComparePic.tsx
+++ components/ComparePic.tsx
@@ -8,8 +8,8 @@ export function ComparePic() {
       <Compare
         firstImage="/OB_logo.jpg"
         secondImage="https://assets.aceternity.com/code-solution.png"
-        firstImageClassName="object-cover object-left-top"
-        secondImageClassname="object-cover object-left-top"
+        firstImageClassName="object-cover object-center"
+        secondImageClassname="object-cover object-center"
         className="h-[300px] w-[250px] md:h-[350px] md:w-[350px]"
         slideMode="hover"
       />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,10 +39,10 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-1  z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Orel Bukris portfolio</h2>
+            <h2 className=" tracking-widest text-xs text-center text-blue-100 max-w-80">Orel Bukris | Building Engaging User Interfaces</h2>
             <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:test-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Turning Ideas into Smooth User Experiences"
             />
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
                 Hi, I&apos;m Orel. I&apos;m a Front-End Developer.
