d937983ce8620b4e5d78c2b27ca448524a952437
Orel bukris <orelbukris7777@gmail.com>
2024-10-30 15:56:39 +0200

    go

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -25,24 +25,24 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title={item.title} 
+              href={item.link}   
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  className="relative w-full h-full overflow-hidden rounded-xl lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
                   <img 
                     src="/bg.png" 
                     alt="bgimg"
-                    className="w-full h-full object-cover" 
+                    className="w-full h-full object-cover " 
                   />
                 </div>
                 <img
                   src={item.img}
                   alt={`${item.title} cover`}
-                  className="z-10 absolute bottom-0 w-auto h-auto"
+                  className="z-10 w-auto h-auto object-cover rounded-lg rounded md:rounded-lg sm:rounded-xl "
                 />
               </div>
 
