56efe7daf1d3973fbe5f68684ba3c6d12845a359
Orel bukris <orelbukris7777@gmail.com>
2024-11-01 15:24:44 +0200

    v

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,16 +30,16 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center ">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright Orel Bukris
+           Orel Bukris Front-End Developer
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-3"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
