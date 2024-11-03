17b9887bb20e82cb81b7691292cbbb330de6e941
Orel bukris <orelbukris7777@gmail.com>
2024-11-03 15:36:09 +0200

    fix1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,6 @@ import { MagicButton } from "./ui/MagicButton";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Are you prepared <span className="text-purple">to elevate</span> your digital presence to the next level?
