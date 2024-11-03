8eeda2e16faaf7bbeaef409566352957445e0f33
Orel bukris <orelbukris7777@gmail.com>
2024-11-03 15:53:49 +0200

    fix2

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
       className="relative bg-black-100 flex justify-center
     items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <ComparePic />
         <Card3D />
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,7 +13,7 @@ const RecentProjects = () => {
   }, []);
 
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
