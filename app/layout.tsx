937951d59be9af2fd813f50fb86ee375137c21f1
Orel bukris <orelbukris7777@gmail.com>
2024-11-12 22:35:05 +0200

    end

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/OB_logo.jpg" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
