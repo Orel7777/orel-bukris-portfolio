2027341a3b73630896b48141f66159ffc16510a1
Orel bukris <orelbukris7777@gmail.com>
2025-03-22 20:23:11 +0200

    go 1

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod.spline.design',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://prod.spline.design;"
+          }
+        ],
+      },
+    ]
+  }
+}
+
+module.exports = nextConfig 
