c7c8e59fefea724088bc19d01871d9baac547996
Orel bukris <orelbukris7777@gmail.com>
2025-04-22 10:33:22 +0300

    תיקון אפקט תלת-ממד בכרטיסים בקומפוננט 3d-pin

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -21,8 +21,8 @@ export const PinContainer = ({
   containerClassName,
   onClick,
 }: PinProps) => {
-  const [, setTransform] = useState(
-    "translate(-50%,-50%) rotateX(0deg)"
+  const [transform, setTransform] = useState(
+    "translate(-50%,-50%) rotateX(0deg) scale(1)"
   );
 
   const onMouseEnter = () => {
@@ -35,7 +35,7 @@ export const PinContainer = ({
   return (
     <Link
       className={cn(
-        "relative group/pin z-50  cursor-pointer",
+        "relative group/pin z-50 cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
@@ -44,8 +44,28 @@ export const PinContainer = ({
       onClick={onClick}
     >
       <div
+        style={{
+          perspective: "1000px",
+          transform: "rotateX(70deg) translateZ(0deg)",
+        }}
+        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
+      >
+        <div
+          style={{
+            transform: transform,
+          }}
+          className="absolute left-1/2 top-1/2 flex justify-start items-start rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-zinc-900 border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden p-4"
+        >
+          <div className={cn("relative z-50", className)}>
+            {title && <h2 className="font-medium text-white text-2xl">{title}</h2>}
+            {children}
+          </div>
+        </div>
+      </div>
+
+      <div
         className={cn(
-          "relative bg-zinc-900 h-full w-full rounded-2xl p-4 overflow-hidden border border-white/[0.08] group-hover:border-white/[0.2] transition duration-700",
+          "relative bg-zinc-900 h-full w-full rounded-2xl p-4 overflow-hidden border border-white/[0.08] group-hover:border-white/[0.2] transition duration-700 invisible",
           className
         )}
         style={{ maxWidth: "100%", maxHeight: "100%" }}
@@ -77,6 +97,7 @@ export const PinContainer = ({
           </div>
         )}
       </div>
+      <PinPerspective title={title} href={href} />
     </Link>
   );
 };
